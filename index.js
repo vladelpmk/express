8da54bd9034c186dcebce90966202495ebac4d44
Vladimir <vladimir.gjorgievski@sytac.io>
2020-02-09 22:54:13 +0100

    initail commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => { // new
+  res.send('<h1>Aleksandra <3</h1>');
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
